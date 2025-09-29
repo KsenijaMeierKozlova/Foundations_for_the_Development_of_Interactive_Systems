bf17dfd29ed354c3a0adc1d1611517b23984870b
meyer.ks <kseniya.meyer@gmail.com>
2025-09-29 19:23:57 +0300

    Lab7 is added

---
+++ Labs/Lab7/script.js
@@ -0,0 +1,16 @@
+let firstName = "Ksenija";
+let newSentence = "Hello, my name is " + firstName;
+
+console.log(newSentence);
+
+let num1 = 6;
+let num2 = 8;
+let result = num1 + num2;
+
+console.log(result);
+
+let mixedResult = firstName + num1;
+console.log(mixedResult);
+
+console.log(newSentence + ", I am called " + firstName + ", call me " + firstName + ", ... " + firstName + ".");
+console.log(`${newSentence}, I am called ${firstName}, call me ${firstName}, ... ${firstName}.`);
