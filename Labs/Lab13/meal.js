9a4c74ed4149f223558325aac4ff69a8ac37a1c8
meyer.ks <kseniya.meyer@gmail.com>
2025-11-10 19:53:30 +0200

    Lab13 was started with async and sync functions explanations

---
+++ Labs/Lab13/meal.js
@@ -0,0 +1,22 @@
+/* 1 - Link to get a random meal: https://www.themealdb.com/api/json/v1/1/random.php */
+
+/* 2 - Link to lookup a specific meal with an id: https://www.themealdb.com/api/json/v1/1/lookup.php?i= */
+
+/* 3 - Link to search for meals using a keyword: https://www.themealdb.com/api/json/v1/1/search.php?s= */
+
+let randomMealURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+/* const getRandomMeal = () => {
+    fetch(randomMealURL)
+    .then((item) => item.json())
+    .then((data) => console.log(data.meals[0]))
+    .catch((err) => console.log("There was issue in the fetching ", err));
+} */
+
+const getRandomMeal = async () => {
+    const resp = await fetch(randomMealURL);
+    const data = await resp.json();
+    console.log(data.meals[0]);
+}
+
+getRandomMeal();
