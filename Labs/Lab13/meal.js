bef9dc4b7fee732ec7d88aed028628c2286ac2a9
meyer.ks <kseniya.meyer@gmail.com>
2025-11-16 22:55:13 +0200

    HW#9 is done!

--- Labs/Lab13/meal.js
+++ Labs/Lab13/meal.js
@@ -13,10 +13,61 @@ let randomMealURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
     .catch((err) => console.log("There was issue in the fetching ", err));
 } */
 
+const mealsElement = document.getElementById("meals");
+
 const getRandomMeal = async () => {
     const resp = await fetch(randomMealURL);
     const data = await resp.json();
-    console.log(data.meals[0]);
+    const randomMeal = data.meals[0];
+
+    mealsElement.innerHTML = "";
+    
+    addMeal(randomMeal);
 }
 
 getRandomMeal();
+
+const addMeal = (mealData) => {
+    const meal = document.createElement('div');
+    meal.classList.add("meal");
+
+    meal.innerHTML = `<div class="meal-header">
+                        <span class="random">Meal of the Day</span>
+                        <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+                    </div>
+                    <div class="meal-body">
+                        <h3>${mealData.strMeal}</h3>
+                            <button class="fav-btn">
+                            <i class="fas fa-heart"></i>
+                        </button>
+                    </div>`;
+    
+    let favouriteButton = meal.querySelector(".fav-btn");
+    favouriteButton.addEventListener("click", () => {
+        if(favouriteButton.classList.contains('active')) {
+            favouriteButton.classList.remove('active')
+        } else {
+            favouriteButton.classList.add('active');
+            addMealToLocalStorage(mealData.idMeal);
+        }
+    })
+    
+    mealsElement.appendChild(meal);
+}
+
+const addMealToLocalStorage = (mealId) => {
+    const mealIds = getMealsFromLocalStorage();
+
+    localStorage.setItem('mealId', JSON.stringify([...mealIds, mealId]));
+}
+
+const removeMealFromLocalStorage = (mealId) => {
+    const mealIds = getMealsFromLocalStorage();
+    localStorage.setItem('mealIds', JSON.stringify(mealIds.filter(id => id != mealId)));
+}
+
+const getMealsFromLocalStorage = () => {
+    const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+
+    return mealIds === null? [] : mealIds;
+}
